daa9a11f46d69bf898fdcf65340bae58dbdbebbe
Rachel-Reidenga <reidenga@gmail.com>
2021-01-04 23:09:26 -0600

    drag style

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,6 +223,7 @@ var dropZoneDragHandler = function(event) {
     if (taskListEl){
         event.preventDefault();
     }
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
 };
 
 var dropTaskHandler = function(event) {
@@ -241,6 +242,14 @@ var dropTaskHandler = function(event) {
        statusSelectEl.selectedIndex = 2;
    }
    dropZoneEl.appendChild(draggableElement);
+   dropZoneEl.removeAttribute("style");
+}
+
+var dragLeaveHandler = function(event){
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -252,3 +261,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
